38ca9ad6b8a5452a46547c8b66532ca6fdd4adfa
moein <Moeinabdi77@gmail.com>
2018-10-21 20:44:20 +0330

    Initial commit

---
+++ app/src/main/java/moeindeveloper/kotlinroomaac/DataSource/Room/KNote.kt
@@ -0,0 +1,7 @@
+package moeindeveloper.kotlinroomaac.DataSource.Room
+
+//data model class for room table and accessing data:
+
+data class KNote(var id: Long?,
+                 var tile: String,
+                 var description: String)
---
+++ app/src/main/java/moeindeveloper/kotlinroomaac/MainActivity.kt
@@ -0,0 +1,12 @@
+package moeindeveloper.kotlinroomaac
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
